61fdc6dc8fb28e15381cb324141b3e5b0ea8b8fb
Likhitha <71483520+LikhithaRajgopal2001@users.noreply.github.com>
2026-04-26 07:06:12 +0530

    Change AMI ID in project_ec2 module
    Updated the AMI ID for the EC2 module configuration.

--- Project/starter/terraform/ec2.tf
+++ Project/starter/terraform/ec2.tf
@@ -11,7 +11,7 @@
    source             = "./modules/ec2"
    name               = local.name
    account            = data.aws_caller_identity.current.account_id
-   aws_ami            = "ami-01040813c3969933e"
+   aws_ami            = "ami-0d528b74c1828712e"
    private_subnet_ids = module.vpc.private_subnet_ids
    vpc_id             = module.vpc.vpc_id
  }
